8f992e769887388616c675f2dda40a7be2852ba1
POOVARASAN-03 <poovarasang1212@gmail.com>
2025-10-24 20:08:19 +0530

    removed new application in navbar in application dashboard

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -44,14 +44,7 @@ const Navbar = () => {
                   </span>
                 </div>
                 
-                {user.role === 'applicant' && (
-                  <Link
-                    to="/applications/create"
-                    className="bg-primary-500 text-white px-4 py-2 rounded-md hover:bg-primary-600 transition-colors"
-                  >
-                    New Application
-                  </Link>
-                )}
+                
                 
                 <button
                   onClick={handleLogout}
